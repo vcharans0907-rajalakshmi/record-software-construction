087fceef7baa4a8f90a3b3d28f61e5b7bc388ff9
vcharans0907-rajalakshmi <vcharans0907@gmail.com>
2026-05-15 09:05:14 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [todo,setTodo]=useState('');
+  const [todoList,setTodoList]=useState([]);
+  const addTodo = () =>{
+    if(todo.trim() === '') return;
+    setTodoList([...todoList,{id:Date.now(),text:todo, completed:false}])
+    setTodo('');
+  }
+  const toggleComplete= (id) =>{
+    setTodoList(
+      todoList.map((item) =>
+        item.id === id? {...item, completed: !item.completed}: item
+      )
+    )
+  }
+  const deleteTodo =(id)=>{
+    setTodoList(
+    todoList.filter((item)=> item.id !== id)
+    )
+  }
+  return (
+    <div className='app-container'>
+      <h1>Todo-List</h1>
+      <div className="input-container">
+        <input type="text"
+        placeholder='Add a new task...'
+        value={todo}
+        onChange={(e)=> setTodo(e.target.value)}
+        />
+        <button className='add-btn' onClick={addTodo}>Add</button>
+
+      </div>
+      <ul className='todo-list'>
+        {todoList.map((item)=>(
+          <li key={item.id} onClick={()=>toggleComplete(item.id)}
+          style={{cursor:'pointer',
+            textDecoration:item.completed? 'linethrough':'none',
+            color:item.completed? 'gray':'black'
+          }}
+          >
+            <span>{item.text}</span>
+            <button className='delete-btn' onClick={(e)=>{
+              e.stopPropagation();
+              deleteTodo(item.id);
+            }}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+export default App
